6eb6e9361066cdc7aaad6c56af0dd3f6511ddc3a
swathi015 <thungathurthiswathi06@gmail.com>
2026-04-24 15:16:41 +0530

    Initial Commit

--- frontend/src/components/ArticleByID.jsx
+++ frontend/src/components/ArticleByID.jsx
@@ -6,7 +6,7 @@ import { toast } from "react-hot-toast";
 import {
   articlePageWrapper,
   articleHeader,
-  articleCategory,
+  articleCategory, 
   articleMainTitle,
   articleAuthorRow,
   authorInfo,
